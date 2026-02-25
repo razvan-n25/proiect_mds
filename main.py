615eb1213f93ebd5dce2a81d36924fba8fd8fdbd
razvan-n25 <razvannica705@yahoo.com>
2026-02-25 12:04:36 +0000

    script si dependente2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 url ="https://api.open-meteo.com/v1/forecast?latitude=44.4323&longitude=26.1063&current_weather=true"
 response=requests.get(url)
-if response.status_code=200:
-data=response.json()
-temp=data["current_weather"]["temperature"]
-print(f"Succes! Temperatura actuala in Bucuresti este de {temp}.")
+if response.status_code==200:
+	data=response.json()
+	temp=data["current_weather"]["temperature"]
+	print(f"Succes! Temperatura actuala in Bucuresti este de {temp}.")
 else:
-print("Eroare la descarcare")
+	print("Eroare la descarcare")
 
 
